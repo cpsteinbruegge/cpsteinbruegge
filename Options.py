b615b36bc3a40d5af1c04bdf9fa3633ad808280a
Chuck Steinbruegge <steinbruegge@ameritech.net>
2025-03-12 08:07:34 -0500

    Latest Options Source

---
+++ Options.py
@@ -0,0 +1,186 @@
+import math
+import json
+import csv
+import os
+import sys
+import datetime
+import tabulate
+import Get_valid_input
+
+
+
+
+class Options:
+    def __init__(self,file_name):
+        self.file_name = file_name
+
+        try:
+            if not os.path.exists(self.file_name):
+                with open(self.file_name, 'w',newline="") as f:
+                  writer = csv.writer(f)
+                  writer.writerow(["Symbol" ,"Date","C/P","Strike","Exp Date","Premium","Contracts", "Total Open Premium","Current Price","Close Cost", "Status", "Profit/Loss"])
+            
+        except Exception as e:
+            print(f"Error initializing CSV file: {e}")
+            sys.exit(1)
+
+    def choose_option(self):
+        while True:
+
+            print("\nOptions Tracker")
+            print("1. Enter new trade")
+            print("2. View trades")
+            print("3. Total Options Cash Profit/Loss")
+            print("4. Exit")
+         
+            choice = input("Enter choice: ")
+         
+            if choice == "1":
+                print("\n")
+                self.enter_data()
+
+            elif choice == "2":
+                print("\n")
+                self.view_trades()
+
+            elif choice == "3":
+                print("\n")
+                self.total_cash_profit_loss()
+
+            elif choice == "4": 
+                print("\n")
+                print("Exiting program.")
+                sys.exit(0)
+            else:
+                print("\n")
+                print("Invalid choice. Please try again.")
+
+    def get_valid_date(self, prompt):
+        while True:
+            date_str = input(prompt)
+            try:
+                date =  datetime.datetime.strptime(date_str, "%m-%d-%Y")
+                return date.date()
+            except ValueError:
+                print("Invalid date. Please try again.")
+
+    def enter_data(self):       
+        
+        proceed = True                                  
+        
+        while proceed:
+            symbol = Get_valid_input.get_valid_symbol("Enter Symbol: ")
+            tradedate = self.get_valid_date("Enter Date (MM-DD-YYYY):")    
+            while True:
+                cp = input("Enter C/P: ").upper()
+                if cp == "C" or cp == "P":
+                    break
+                else:
+                    print("Invalid input. Please enter C or P.")
+
+            strike = Get_valid_input.get_valid_int("Enter Strike: ")
+            exp_date = Get_valid_input.get_valid_date("Enter Exp Date (MM-DD-YYYY): ")
+            premium = Get_valid_input.get_valid_float("Enter Premium ($x.xx): ",2)   
+            contracts = Get_valid_input.get_valid_int("Enter Contracts: ")
+            current_price = Get_valid_input.get_valid_int("Enter Current Price: ")
+            close_cost = Get_valid_input.get_valid_int("Enter Close Cost: ")
+            #Get status
+            while True:
+
+                status = input("Enter Status: O/C: ").upper()
+                if status == "O" or status == "C":
+                    break   
+                else:
+                    print("Invalid input. Please enter (O)pen or (C)losed.")
+             
+            self.log_trade(symbol,tradedate, cp, strike, exp_date, premium, contracts, current_price, close_cost, status)
+            
+            #Ask user if they want to add another trade
+
+            while True:   
+               answer = input("Add another trade? Y/N ").upper()   
+               if answer in {'Y','N'}:
+                   break
+               else:
+                   print("Invalid input. Please enter Y or N.")
+
+            if answer == "N":
+               proceed = False  
+            elif answer == "Y":
+               proceed = True           
+              
+    def log_trade (self,symbol, tradedate, cp, strike, exp_date, premium, contracts, current_price, close_cost, status,):
+        try:
+            total_open_premium= float(premium) * int(contracts) * 100
+            profit_loss = int(total_open_premium - close_cost)
+            with open(self.file_name, mode='a', newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow([symbol, tradedate, cp, strike, exp_date, premium, contracts, f"{total_open_premium:.0f}" , int(current_price), close_cost, status, f"{int(profit_loss)}"])
+                #debug data
+                #print("Data WRITTEN TO FILE", [symbol, tradedate, cp, strike, exp_date, premium, contracts, f"{total_open_premium:.0f}" , int(current_price), close_cost, status, f"{int(profit_loss)}"])
+                #end debug
+        except Exception as e:
+            print(f"Error logging trade: {e}")
+            sys.exit(1)
+        
+
+    def view_trades(self):
+        try:
+            if not os.path.exists(self.file_name):
+                  print("No trade records found.")
+                  return
+            with open(self.file_name , mode='r') as file:
+                reader = csv.reader(file)
+                data = list(reader)
+                #debug data
+                
+                #print("Data READ FROM FILE", data)
+
+                #end debug
+
+            if len(data) > 1:
+               headers =  ["No."] + data[0] 
+               rows = [[i] + row for i,row in enumerate(data[1:],start = 1)]
+               #print("\n")  
+               print(tabulate.tabulate(rows, headers=headers,tablefmt='pipe'))               
+               
+            else:
+                print("No trade records found.")
+
+        except Exception as e:
+            print(f"Error viewing trades: {e}")
+            sys.exit(1)   
+
+    def total_cash_profit_loss(self):        
+        try:
+            if not os.path.exists(self.file_name):
+                print("No trade records found.")    
+                return
+            with open(self.file_name, mode='r') as file:
+                reader = csv.reader(file)
+                data = list(reader)
+                total_profit_loss = 0
+                for row in data[1:]:
+                    if len(row) < 12:  # Skip rows that don't have profit/loss
+                       print("Skipping incomplete row", row)
+                       continue
+
+                    try:
+                      total_profit_loss += int(row[-1])
+                    except ValueError:
+                        print(f"Skipping invalid profit/loss value in row", {row[-1]})
+                        continue
+                print(f"Total Options Cash Profit/Loss: {total_profit_loss}")
+        except Exception as e:
+            print(f"Error calculating total profit/loss: {e}")
+            sys.exit(1)
+def main():
+    file_name = "Options.csv"
+    OptionsTracker = Options(file_name)
+    OptionsTracker.choose_option()
+    
+
+
+
+if __name__ == "__main__":
+    main()
